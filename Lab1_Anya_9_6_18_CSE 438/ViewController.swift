f1bdd07077021a172601f214c53c22f7b39f1eeb
Anya Pawar <anya.pawar@wustl.edu>
2019-09-06 21:41:01 -0500

    Added var

--- Lab1_Anya_9_6_18_CSE 438/ViewController.swift
+++ Lab1_Anya_9_6_18_CSE 438/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var person = "Anya"
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
